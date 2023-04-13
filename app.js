45789f4d6368a312113a53f255b14d2386857f6e
Anastasiia Bilska <nastya.bilskaya@gmail.com>
2023-04-13 19:10:23 +0300

    update

--- app.js
+++ app.js
@@ -1,31 +1,37 @@
-'use strict';
+"use strict";
 
 const urlParams = new URLSearchParams(window.location.search);
-const channel = urlParams.get('channel');
-const phone = urlParams.get('phone');
+const channel = urlParams.get("channel");
+const phone = urlParams.get("phone");
 
 // if (channel === 'telegram') {
-  window.Telegram.WebApp.expand();
+window.Telegram.WebApp.expand();
 // }
 
 let scanerObj, lastCode;
 
-const messagesForTelegramScan = ['Аптека 9-1-1 бажає вам гарного дня!', 'Аптека 9-1-1 бажає вам міцного здоров\'я', 'Аптека 9-1-1 дуже вдячна за ваш труд!']
+const messagesForTelegramScan = [
+  "Аптека 9-1-1 бажає вам гарного дня!",
+  "Аптека 9-1-1 бажає вам міцного здоров'я",
+  "Аптека 9-1-1 дуже вдячна за ваш труд!",
+];
 const randomIndex = Math.floor(Math.random() * strings.length);
 const randomMessage = messagesForTelegramScan[randomIndex];
 
 function openTgLink() {
-  window.Telegram.WebApp.openTelegramLink("index-dynamsoft.html");	
-  // openLink("index-dynamsoft.html", try_instant_view = true);
+  // window.Telegram.WebApp.openTelegramLink("index-dynamsoft.html");
+  window.Telegram.WebApp.openLink(
+    "index-dynamsoft.html"
+  );
 }
 
 // запуск сканера
 async function showScaner() {
-  console.log('START OLD');
+  console.log("START OLD");
   lastCode = null;
 
   try {
-    document.getElementById('loader-wrapper').classList.remove('hide');
+    document.getElementById("loader-wrapper").classList.remove("hide");
 
     // if (channel === 'telegram') {
     //   window.Telegram.WebApp.showScanQrPopup({
@@ -33,8 +39,11 @@ async function showScaner() {
     //   });
     //   window.Telegram.WebApp.onEvent('qrTextReceived', scanerResult);
     // } else {
-      setTimeout(() => {
-      if (scanerObj && scanerObj.getState() === Html5QrcodeScannerState.PAUSED) {
+    setTimeout(() => {
+      if (
+        scanerObj &&
+        scanerObj.getState() === Html5QrcodeScannerState.PAUSED
+      ) {
         scanerObj.resume();
       } else {
         if (
@@ -67,13 +76,13 @@ async function showScaner() {
           .catch(function (e) {
             console.log(e);
           });
-          
-        // setTimeout(() => {
-        }
-        document.getElementById("loader-wrapper").classList.add("hide");
-      }, 1500);
 
-      // }
+        // setTimeout(() => {
+      }
+      document.getElementById("loader-wrapper").classList.add("hide");
+    }, 1500);
+
+    // }
     // document.getElementById('loader-wrapper').classList.add('hide');
     // }
   } catch (e) {
@@ -102,8 +111,10 @@ function scanerResult(code) {
     return;
   }
 
-  if (channel === 'telegram') {
-    window.Telegram.WebApp.showAlert("QR успішно відскановано ✅\n Перевіряємо інформацію ⏳");
+  if (channel === "telegram") {
+    window.Telegram.WebApp.showAlert(
+      "QR успішно відскановано ✅\n Перевіряємо інформацію ⏳"
+    );
   }
 
   // alert('QR успішно відскановано ✅\n Перевіряємо інформацію ⏳');
@@ -114,42 +125,42 @@ function scanerResult(code) {
   }, 1500);
 
   // отправляем данные на api - новий
-    // let xhr = new XMLHttpRequest();
-    // xhr.open('POST', {{{msg.serverUrl}}}'/QRres', true);
-    // xhr.setRequestHeader('Content-Type', 'application/json');
+  // let xhr = new XMLHttpRequest();
+  // xhr.open('POST', {{{msg.serverUrl}}}'/QRres', true);
+  // xhr.setRequestHeader('Content-Type', 'application/json');
 
-    // if (channel === 'telegram') {
-    //   xhr.send(
-    //     JSON.stringify({
-    //       text: code,
-    //       channel: `{{{payload.channel}}}`,
-    //       phone: `{{{payload.phone}}}`,
-    //       user: window.Telegram.WebApp.initDataUnsafe.user,
-    //     })
-    //   );
-    // } else if (channel === 'viber') {
-    //   xhr.send(
-    //     JSON.stringify({
-    //       text: code,
-    //       channel: `{{{payload.channel}}}`,
-    //       phone: `{{{payload.phone}}}`,
-    //     })
-    //   );
-    // }
+  // if (channel === 'telegram') {
+  //   xhr.send(
+  //     JSON.stringify({
+  //       text: code,
+  //       channel: `{{{payload.channel}}}`,
+  //       phone: `{{{payload.phone}}}`,
+  //       user: window.Telegram.WebApp.initDataUnsafe.user,
+  //     })
+  //   );
+  // } else if (channel === 'viber') {
+  //   xhr.send(
+  //     JSON.stringify({
+  //       text: code,
+  //       channel: `{{{payload.channel}}}`,
+  //       phone: `{{{payload.phone}}}`,
+  //     })
+  //   );
+  // }
 
-    // отправляем данные на api - початковий
-    // let xhr = new XMLHttpRequest();
-    // xhr.open("POST", {{{msg.serverUrl}}}'/QRres', true);
-    // xhr.setRequestHeader('Content-Type', 'application/json');
-    // xhr.send(JSON.stringify({
-    //     "text": code,
-    //     "channel": `{{{payload.channel}}}`,
-    //     "phone": `{{{payload.phone}}}`,
-    //     "user": window.Telegram.WebApp.initDataUnsafe.user
-    // }));
+  // отправляем данные на api - початковий
+  // let xhr = new XMLHttpRequest();
+  // xhr.open("POST", {{{msg.serverUrl}}}'/QRres', true);
+  // xhr.setRequestHeader('Content-Type', 'application/json');
+  // xhr.send(JSON.stringify({
+  //     "text": code,
+  //     "channel": `{{{payload.channel}}}`,
+  //     "phone": `{{{payload.phone}}}`,
+  //     "user": window.Telegram.WebApp.initDataUnsafe.user
+  // }));
 
-    // редирект
-    // setTimeout(redirect, 500);
+  // редирект
+  // setTimeout(redirect, 500);
 
   setTimeout(redirect, 500);
 
@@ -157,10 +168,10 @@ function scanerResult(code) {
 }
 
 function redirect() {
-  if (channel === 'telegram') {
+  if (channel === "telegram") {
     window.Telegram.WebApp.close();
-  } else if (channel === 'viber') {
-    window.location.replace(decodeURIComponent('{{payload.redirectLink}}'));
+  } else if (channel === "viber") {
+    window.location.replace(decodeURIComponent("{{payload.redirectLink}}"));
   }
 
   window.Telegram.WebApp.close();
