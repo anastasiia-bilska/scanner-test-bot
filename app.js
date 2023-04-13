57dc2b3c673c9d7b8081b8d78bccb796e652f8fe
Anastasiia Bilska <nastya.bilskaya@gmail.com>
2023-04-13 18:42:02 +0300

    update

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ const randomIndex = Math.floor(Math.random() * strings.length);
 const randomMessage = messagesForTelegramScan[randomIndex];
 
 function openTgLink() {
-  // openTelegramLink("index-dynamsoft.html");	
-  openLink("index-dynamsoft.html", try_instant_view = true);
+  window.Telegram.WebApp.openTelegramLink("index-dynamsoft.html");	
+  // openLink("index-dynamsoft.html", try_instant_view = true);
 }
 
 // запуск сканера
