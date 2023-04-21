7aeb26e10a488d20d08003a4dc848ce064e9245b
Anastasiia Bilska <nastya.bilskaya@gmail.com>
2023-04-21 15:10:01 +0300

    vzayemodiia

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 window.Telegram.WebApp.expand();
 const ct = window.Telegram.WebApp.colorScheme;
 
+window.Telegram.WebApp.showScanQrPopup({
+  text: "Помістіть QR-код у центр екрану 🎯",
+});
+window.Telegram.WebApp.onEvent("qrTextReceived", scanerResult);
+
 window.console.log(ct);
