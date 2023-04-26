5055a88377b4f705c5adacc9e107f969960ba3fc
Anastasiia Bilska <nastya.bilskaya@gmail.com>
2023-04-26 08:44:09 +0300

    update

--- app.js
+++ app.js
@@ -502,6 +502,8 @@ async function showScaner() {
     } catch (e) {
       let err;
 
+      alert(e.message)
+
       if (e.message.includes('network connection error')) {
         err = 'Failed to connect to Dynamsoft License Server';
       } else {
